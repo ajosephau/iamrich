a33cb78f03dc3ff62c703f8cc3b6e446c863f107
Anthony Joseph <ajosephau@users.noreply.github.com>
2023-12-27 06:41:07 +1100

    add image to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Center(child: Text("Hello world"))));
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "I am rich",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.blue[900],
+      ),
+      backgroundColor: Colors.blue[300],
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+              "https://images.unsplash.com/photo-1502514841534-dceabea28dad"),
+          alignment: Alignment.bottomCenter,
+        ),
+      ),
+    ),
+  ));
 }
