256f8fbd1f14f2fda061528c2bfbd1e5846a91cc
Anthony Joseph <ajosephau@users.noreply.github.com>
2023-12-27 15:39:37 +1100

    add image to app, deploy to phone

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,22 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text(
-          "I am rich",
-          style: TextStyle(
-            color: Colors.white,
+        title: Center(
+          child: Text(
+            "I am rich",
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
         ),
-        backgroundColor: Colors.blue[900],
+        backgroundColor: Colors.blueGrey[900],
       ),
-      backgroundColor: Colors.blue[300],
+      backgroundColor: Colors.yellow[100],
       body: Center(
         child: Image(
-          image: NetworkImage(
-              "https://images.unsplash.com/photo-1502514841534-dceabea28dad"),
+          image: AssetImage(
+              "images/bg.jpg"
+          ),
           alignment: Alignment.bottomCenter,
         ),
       ),
